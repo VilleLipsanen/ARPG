c10b2e66cd72ce18f26772e707946e4b74e074ea
Lasse <34071995+p3kka123@users.noreply.github.com>
2019-07-24 13:57:43 +0300

    attack update
    attack takes stats into account

--- ARPG/Assets/Scripts 1/HPBehavior.cs
+++ ARPG/Assets/Scripts 1/HPBehavior.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HPBehavior : MonoBehaviour
 {
 
-    int HP = 100;
+    public int HP = 100;
 
     // Start is called before the first frame update
     void Start()
